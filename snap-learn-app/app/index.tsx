3a11315bcc42df5099572b8abd1e6331378cc788
tegar_3829 <136164236+adipiscin@users.noreply.github.com>
2025-07-24 18:30:26 +0700

    Installed nativewind

--- snap-learn-app/app/index.tsx
+++ snap-learn-app/app/index.tsx
@@ -1,15 +1,22 @@
+import React from "react";
 import { Text, View } from "react-native";
+import "./global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    // <ScrollView
+    //   contentContainerStyle={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Text>Edit app/index.tsx to edit this screen.</Text>
+    // </ScrollView>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Tailwind of the project
+      </Text>
     </View>
   );
 }
